58ff250a4e9d0edefd0ff2c682fa00bbe2410421
Sarah <sarah.oconnor@webinnovate.ie>
2015-04-24 16:41:20 +0100

    add ngStorage

--- todo.js
+++ todo.js
@@ -1,41 +1,51 @@
 
-	angular.module('ToDoApp',[]).controller('TodoController',['$scope', function($scope){
+	angular.module('ToDoApp',['ngStorage']).controller('TodoController',['$scope', '$localStorage', function($scope, $localStorage){
 		
 		
 
+		$scope.$storage = $localStorage.$default ({
+		
 
-		$scope.todo = [
-			{'title':'Build a To Do App', done: false}//start with empty array so as not to start with a lost checkbox
+		todo : [
+			//{'title':'Build a To Do App', done: false}//start with empty array so as not to start with a lost checkbox
 			//if(!angular.isObject(todo.title) todo=[];
-		]; 
+		]
 
 		//emptyTodo = function () {  //hides checkbox but still counts as one todo
 
 		//angular.isObject(todo.title) todo=[];
 		//}
-
+		});
 
 
 		$scope.addTodo = function () {
-			$scope.todo.push({title: $scope.newTodo, done: false});
+			$scope.$storage.todo.push({title: $scope.newTodo, done: false});
 				$scope.newTodo = '';
-				//LocalStorage.setItem('todo', JSON.stringify(todo));
+				
 				}//close addTodo function 
 
 		$scope.remaining = function() {
 			var count = 0;
-			angular.forEach($scope.todo, function(currentTodo){  //angular.forEach iterator function 
+			angular.forEach($scope.$storage.todo, function(currentTodo){  //angular.forEach iterator function 
 				count += currentTodo.done ? 0 : 1;  
 			});
 			return count;
 		}
 
-		$scope.hideDone = function () {
-			var oldTodo = $scope.todo;
-			$scope.todo = [];//creat empty array
-			angular.forEach(oldTodo, function(currentTodo){
-				if (!currentTodo.done) $scope.todo.push(currentTodo);// create array of  undone todos from original array
-			});
-			//LocalStorage.setItem('todo', JSON.stringify(todo));
-		}
+		
+
+
+
+	
+
+		$scope.deleteDone = function () {
+			var i = $scope.$storage.todo.length;
+			while (i--) {
+				if ($scope.$storage.todo[i].done){
+					$scope.$storage.todo.splice(i,1);
+				}
+			}
+		}	
+
+
 }])
